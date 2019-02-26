ae586b8b0388e776d88cbd929d4a82d2353caa8d
alihhussain <alihhussain@live.com>
2019-02-26 13:29:06 -0800

    add

--- ACS-Engine_Testing/enhanced_sampleContainer/app.py
+++ ACS-Engine_Testing/enhanced_sampleContainer/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 from fabric.api import local
 import os
+import socket
 
 runtime = str("None")
 try:
@@ -12,17 +13,17 @@ try:
     runtime = str("Kubernetes")
 except:
     runtime = str("Docker")
-IP_output = str(local("""ip -o -4 a | awk '$2 == "eth0" { gsub(/\/.*/, "", $4); print $4 }'""", capture=True))
+    
 ID_output = str(local("""cat /proc/self/cgroup | head -n 1 | cut -d '/' -f3""", capture=True))
-
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     if 'docker' in runtime.lower():
-        return render_template("docker_index.html", container_IP=IP_output, container_ID=ID_output)
+        return render_template("docker_index.html", container_IP=str(socket.gethostbyname(socket.gethostname())), container_ID=ID_output, remote_IP=str(request.remote_addr), HostName=str(socket.gethostname()))
     else:
-        return render_template("k8s_enhance_index.html", k8s_node_name=k8s_node_name, k8s_pod_name=k8s_pod_name, k8s_pod_namespace=k8s_pod_namespace, k8s_pod_ip=k8s_pod_ip, k8s_serviceaccount_name=k8s_serviceaccount_name)
+        return render_template("k8s_enhance_index.html", k8s_node_name=k8s_node_name, k8s_pod_name=k8s_pod_name, k8s_pod_namespace=k8s_pod_namespace, remote_IP=str(request.remote_addr),k8s_pod_ip=k8s_pod_ip, k8s_serviceaccount_name=k8s_serviceaccount_name)
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=80)
